3f1b27d0a7b34ef94032fda93317a688f5f2dd65
mttasdemir <mt.tasdemir@gmail.com>
2022-10-23 16:58:50 +0300

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Mustafa Taşdemir on 23.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
